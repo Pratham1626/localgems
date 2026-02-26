4b76c308c3a652cfabb1cd6956bfbc081f31f7c5
Pratham1626 <prathamdodiya1626@gmail.com>
2026-02-26 12:31:37 +0530

    localgems

---
+++ core/forms.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+from django import forms
+
+class UserSignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['email', 'firstname', 'lastname', 'gender', 'mobile', 'role', 'password1', 'password2']
+        widgets = {
+            'gender': forms.RadioSelect(),
+            'password1': forms.PasswordInput(),
+            'password2': forms.PasswordInput(),
+        }
+
+class UserLoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput())
+    
---
+++ gems/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .decorators import role_required
+
+# Create your views here.
+#@login_required(login_url="login") #check in core.urls.py login name should exist..
+@role_required(allowed_roles=["owner"]) #check in core.urls.py login name should exist..
+def ownerDashboardView(request):
+    return render(request,"gems/owner/owner_dashboard.html")
+
+#@login_required(login_url="login")
+@role_required(allowed_roles=["user"]) #check in core.urls.py login name should exist.. 
+def userDashboardView(request):
+    return render(request,"gems/user/user_dashboard.html")
